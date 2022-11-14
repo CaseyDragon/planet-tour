d7d0912d5dd5c9ca62b9f74871b4a5413a3d6b9f
CaseyDragon <cndragon83@gmail.com>
2022-11-14 11:54:49 -0500

    commented out Componenets

--- src/Components/PlanetName.js
+++ src/Components/PlanetName.js
@@ -5,9 +5,9 @@ function PlanetName() {
     return(
         <div>
             <h1>planets 1-8</h1>
-        // hopefully will iterate through api to get all the names
+        {/* hopefully will iterate through api to get all the names */}
         <Planet />
-        this will be a link to the planet i believe
+        {/* this will be a link to the planet i believe */}
         </div>
      )
 }
